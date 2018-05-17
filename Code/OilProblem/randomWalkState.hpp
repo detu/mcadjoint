fe45e62f1d9a0bde427070b1393cb4a6ea764884
Stefano Weidmann <stefanow@student.ethz.ch>
2018-05-17 11:14:40 +0200

    Todo: antithetic random walks

--- Code/OilProblem/randomWalkState.hpp
+++ Code/OilProblem/randomWalkState.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 #include "cellindex.hpp"
+#include <memory>
 struct RandomWalkState {
     CellIndex cell;
     bool isAPressure;
@@ -11,4 +12,7 @@ struct RandomWalkState {
     Real W;
     Real D;
     int parameterIndex;
+    constexpr static bool enableAntitheticRandomWalks = true;
+    std::unique_ptr<RandomWalkState> antitheticRandomWalk;
+    #error "TODO antithetic states, put constexpr into a separate header"
 };
